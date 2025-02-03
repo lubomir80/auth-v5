c18373d5e4c894738ac9371f55718a461e3dc821
Lubomir <100048089+lubomir80@users.noreply.github.com>
2025-02-03 14:09:34 +0100

    callback jwt, session, signIn settings

---
+++ data/account.ts
@@ -0,0 +1,16 @@
+import { prisma } from "@/prisma/prisma";
+
+export const getAccountByUserId = async (userId: string) => {
+   try {
+      const account = await prisma.account.findFirst({
+         where: {
+            userId: userId
+         }
+      })
+      return account
+
+   } catch (error) {
+      console.error(error)
+      return null
+   }
+}
---
+++ data/user.ts
@@ -0,0 +1,16 @@
+import { prisma } from "@/prisma/prisma";
+
+export const getUserById = async (id: string) => {
+   try {
+      const user = await prisma.user.findUnique({
+         where: {
+            id: id
+         }
+      })
+      return user
+
+   } catch (error) {
+      console.error(error)
+      return null
+   }
+}
